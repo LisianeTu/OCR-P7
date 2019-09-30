7ae57ca5d4bcd6fc1b30d92444272fd7fc0def20
Lisiane Tu <lisiane.turlure@gmail.com>
2019-09-30 19:00:00 +0200

    addition of restaurant display function

--- src/restaurants.js
+++ src/restaurants.js
@@ -1,7 +1,8 @@
 export { getRestList };
 
 class Restaurant {
-	constructor(name, address, lat, lng, ratings) {
+	constructor(index, name, address, lat, lng, ratings) {
+		this.index = index,
 		this.name = name,
 		this.address = address,
 		this.lat = lat,
@@ -9,14 +10,41 @@ class Restaurant {
 		this.ratings = ratings
 	}
 
+	calculateRating() {
+		const ratings = this.ratings;
+		let averageRating= 0;
+		ratings.forEach((element) => {
+			averageRating += element.stars;
+		})
+		averageRating /= ratings.length; 
+		return averageRating = Math.round(averageRating * 2) / 2;
+	}
+
 	displayRestaurant() {
-		const newLi = document.createElement('li');
-		const info = document.createElement('div');
-		const name = document.createTextNode(this.name + this.address);
-		//const address = document.createTextNode(this.address);
-		info.appendChild(name);
-		newLi.appendChild(info);
-		document.getElementById('list').appendChild(newLi);
+		const newCard = document.createElement('div');
+		newCard.classList.add('card', 'border-top-0', 'border-left-0', 'border-right-0', 'border-bottom', 'rounded-0');
+		document.getElementById('accordionList').appendChild(newCard);
+		const rate = this.calculateRating();
+
+		newCard.insertAdjacentHTML('afterbegin',
+		`<div class="card-header btn btn-link border-0 bg-transparent text-reset text-decoration-none" id="header-${this.index}" data-toggle="collapse" data-target="#collapse-${this.index}">
+				<div class="row justify-content-between">
+					<span class="font-weight-bold">${this.name}</span>
+					<span class="ratings text-right">${rate}</span>
+				</div>
+				<div class="row">
+					<span>${this.address}</span>
+				</div>
+			</div>
+			<div id="collapse-${this.index}" class="collapse" aria-labelledby="header-${this.index}" data-parent="#accordionList">
+				<div class="card-body">
+					<div class="img-street-view">
+					</div>
+					<ul class="list-group-flush p-0">
+					</ul>
+				</div>
+			</div>`
+		);
 	}
 } 
 
@@ -32,9 +60,8 @@ function getRestList() {
 		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { 
 			const restList = xhr.response;
 
-			restList.forEach(element => {
-				const rest = new Restaurant(element.restaurantName, element.address, element.lat, element.long, element.ratings);
-				console.log(rest);
+			restList.forEach((element, i) => {
+				const rest = new Restaurant(i, element.restaurantName, element.address, element.lat, element.long, element.ratings);
 				rest.displayRestaurant();
 			});
 		}
